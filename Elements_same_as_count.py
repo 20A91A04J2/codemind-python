6e46a379fff739430ee1cea3d273b0ceab7e8056
ANAPARTHI SUBRAHMANYA ADITHYA <84435437+20A91A04J2@users.noreply.github.com>
2022-08-20 21:43:29 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in a:     if i not in b:         b.append(i) d=[] for j in range(len(b)):     c=a.count(b[j])     d.append(c) count=0 e=[] for k in range(len(d)):     if(int(d[k])==int(b[k])):         e.append(b[k]) if(len(e)!=0):     print(*e) else:     print("-1")

---
+++ Elements_same_as_count.py
@@ -0,0 +1,20 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+for i in a:
+    if i not in b:
+        b.append(i)
+d=[]
+for j in range(len(b)):
+    c=a.count(b[j])
+    d.append(c)
+count=0
+e=[]
+for k in range(len(d)):
+    if(int(d[k])==int(b[k])):
+        e.append(b[k])
+if(len(e)!=0):
+    print(*e)
+else:
+    print("-1")
+        
