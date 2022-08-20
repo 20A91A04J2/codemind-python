5ab253bd0c72c94335223f00e0585071031d49bb
ANAPARTHI SUBRAHMANYA ADITHYA <84435437+20A91A04J2@users.noreply.github.com>
2022-08-20 20:18:38 +0530

    n=int(input()) a=list(map(int,input().split())) count=0 j=0 for i in range(n):     if(a[i]%2==0):         j=i+2         if(j<n):             if(a[j]%2==0):                 if(a[j-1]%2!=0):                     count=count+1 print(count)

---
+++ count_odd_numbers_between_even_numbers.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+count=0
+j=0
+for i in range(n):
+    if(a[i]%2==0):
+        j=i+2
+        if(j<n):
+            if(a[j]%2==0):
+                if(a[j-1]%2!=0):
+                    count=count+1
+print(count)
+                
